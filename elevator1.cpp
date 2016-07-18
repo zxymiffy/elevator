f6edcaf89175a20b81c5ff97347f01273451d784
zxymiffy <cicizhuang@Godblessme.cscw.cs>
2016-07-18 02:41:57 -0400

    1.0及简版电梯模拟

---
+++ elevator1.cpp
@@ -0,0 +1,18 @@
+//1.0及简版电梯模拟
+//电梯限载量为1人；按时间顺序接送乘客
+#include<cstdio>
+#include<stack>
+#include<queue>
+
+using namespce std;
+
+struct client {
+	int id,infloor,outfloor,whenin,direc;
+}c[1010];/*乘客信息*/
+struct elevator {
+	stack <>
+}
+
+
+int main(){
+}
